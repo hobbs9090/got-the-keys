5f81ddf77fdbc737a0b7920936824fb295675bad
Steven Hobbs <hobbs90@me.com>
2016-01-29 15:46:27 +0000

    Added tests for main pages and changed page title to 'Properties'

--- test/stories/features/step_definitions/my_steps.rb
+++ test/stories/features/step_definitions/my_steps.rb
@@ -1,7 +1,25 @@
-Given(/^I am on homepage$/) do
+Given(/^I visit Homepage$/) do
   visit Homepage
+end
+
+Given(/^I visit Properties$/) do
+  visit Properties
+end
+
+Given(/^I visit For Sale/) do
+  visit ForSale
+end
+
+Given(/^I visit For Rent$/) do
+  visit ForRent
+end
+
+Given(/^I visit For Search$/) do
+  visit Search
 end
 
 Then(/^page is viewable$/) do
   @current_page.assert_page_title
 end
+
+
